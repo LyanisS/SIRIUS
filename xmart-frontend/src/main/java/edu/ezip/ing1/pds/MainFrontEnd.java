6ea54d469c5509466aa5fa9d7e529fc4a5611f2f
Lyanis Souidi <lyanis.souidi@u-pec.fr>
2025-02-21 09:29:02 +0100

    Suppression code inutile

--- xmart-frontend/src/main/java/edu/ezip/ing1/pds/MainFrontEnd.java
+++ xmart-frontend/src/main/java/edu/ezip/ing1/pds/MainFrontEnd.java
@@ -1,15 +1,3 @@
-/*import de.vandermeer.asciitable.AsciiTable;
-import de.vandermeer.asciithemes.a7.A7_Grids;
-import edu.ezip.ing1.pds.business.dto.Student;
-import edu.ezip.ing1.pds.business.dto.Students;
-import edu.ezip.ing1.pds.client.commons.ClientRequest;
-import edu.ezip.ing1.pds.client.commons.ConfigLoader;
-import edu.ezip.ing1.pds.client.commons.NetworkConfig;
-import edu.ezip.ing1.pds.services.TrainService;
-import org.slf4j.LoggerFactory;
-import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.Deque;*/
 package edu.ezip.ing1.pds;
 
 import edu.ezip.ing1.pds.gui.TrainTableFrame;
