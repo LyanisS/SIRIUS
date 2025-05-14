5d267eb5f50d3ff00d2d3fe275e7e3e46bc1f6ae
CHALA El yasmine <celyasmine@gmail.com>
2025-05-15 00:44:06 +0200

    Ajout Select AllTripsClientRequest

---
+++ xmart-frontend/src/main/java/edu/ezip/ing1/pds/requests/SelectAllTripsClientRequest.java
@@ -0,0 +1,28 @@
+package edu.ezip.ing1.pds.requests;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.ezip.ing1.pds.business.dto.Trip;
+import edu.ezip.ing1.pds.business.dto.Trips;
+import edu.ezip.ing1.pds.client.commons.ClientRequest;
+import edu.ezip.ing1.pds.client.commons.NetworkConfig;
+import edu.ezip.ing1.pds.commons.Request;
+
+public class SelectAllTripsClientRequest extends ClientRequest<Trip, Trips> {
+
+    public SelectAllTripsClientRequest(
+            NetworkConfig networkConfig, int myBirthDate, Request request, Trip info, byte[] bytes)
+            throws IOException {
+        super(networkConfig, myBirthDate, request, info, bytes);
+    }
+
+    @Override
+    public Trips readResult(String body) throws IOException {
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return mapper.readValue(body, Trips.class);
+    }
+} 
