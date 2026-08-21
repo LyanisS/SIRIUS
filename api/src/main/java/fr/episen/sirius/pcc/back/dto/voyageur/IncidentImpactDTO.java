b575f3422da9d5f9d8c5fa33c3f09f677deb42cc
El yasmine CHALA <celyasmine@gmail.com>
2026-08-21 11:19:18 +0200

    Revert "Refactor IncidentImpactDTO"
    This reverts commit 48952f51d0369535c6c1a82d45dde1934beef8cc.

--- api/src/main/java/fr/episen/sirius/pcc/back/dto/voyageur/IncidentImpactDTO.java
+++ api/src/main/java/fr/episen/sirius/pcc/back/dto/voyageur/IncidentImpactDTO.java
@@ -1,8 +1,12 @@
 package fr.episen.sirius.pcc.back.dto.voyageur;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class IncidentImpactDTO {
     private Long id;
     private String message;
